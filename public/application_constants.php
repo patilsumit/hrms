ff3f21ca0d67b2b65176505044dd8a28570c371d
patilsumit <patilsumit2020@gmail.com>
2019-10-17 10:23:51 +0530

    first commit

---
+++ public/application_constants.php
@@ -0,0 +1,4 @@
+<?php
+       defined('SUPERADMIN_EMAIL') || define('SUPERADMIN_EMAIL','patilsumit2020@gmail.com');
+       defined('APPLICATION_NAME') || define('APPLICATION_NAME','Sentrifugo');
+     ?>
